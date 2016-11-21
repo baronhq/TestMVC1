65fc807d74235cba39b1762a79b709f45072aadd
baronhq <26222521@qq.com>
2016-11-21 10:39:25 +0800

    1

---
+++ TestMVC1/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVC1.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Browse(string genre)
+        {
+            string msg = HttpUtility.HtmlEncode("name=" + genre);
+            return msg;
+        }
+
+        public string Details(string text, int id)
+        {
+            string msg = text + ";id=" + id;
+            return msg;
+        }
+    }
+}
